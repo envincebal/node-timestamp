02b5d07c128cac6ac129e5bfad037627b93234a6
envincebal <vincentyan1@gmail.com>
2018-08-04 15:56:20 -0700

    Added route that returns current time

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ app.get('/', (req, res) => {
 
 // This function returns UNIX timestamp
 const getUnix = (param) => {
-  if (param.includes(" ")) {
-    return Date.parse(parseInt(param));
+  if (param.includes(" ", "-", "/")) {
+    return Date.parse(param);
   } else {
     return parseInt(param);
   }
@@ -20,18 +20,18 @@ const getUnix = (param) => {
 
 // This function returns date in UTC format
 const getUTC = (param) => {
-  if (param.includes(" ")) {
+  if (param.includes(" ", "-", "/")) {
     return new Date(param).toUTCString();
   } else {
     return new Date(parseInt(param)).toUTCString();
   }
 }
 
-// This route handles user input of either a date or UNIX timestamp
+// This route handles user input of either a date or UNIX timestamp and returns the result
 app.get("/api/timestamp/:date_string", (req, res) => {
   let parameters = req.params.date_string;
   let obj;
-
+  console.log(parameters);
   // This check determines whether "obj" object has an error or not
   if (getUnix(parameters) === null || getUTC(parameters) === "Invalid Date") {
     obj = {
@@ -44,7 +44,14 @@ app.get("/api/timestamp/:date_string", (req, res) => {
     }
   }
   res.send(obj);
+})
 
+// This route sends back current time in Unix and UTC format if there is no user input
+app.get("/api/timestamp/", (req, res) => {
+  res.send({
+    unix: Date.now(),
+    utc: new Date().toUTCString()
+  });
 })
 
 // This listens for a server
